bf34cd72d5fa8273338ad50e9881796489e7fa2c
HugoChollet <tibolescabo@gmail.com>
2023-03-21 11:27:15 +0100

    add DayView and MonthView

--- src/components/Agenda/agenda.tsx
+++ src/components/Agenda/agenda.tsx
@@ -3,17 +3,25 @@ import Paper from "@mui/material/Paper";
 import { ViewState } from "@devexpress/dx-react-scheduler";
 import {
   Scheduler,
+  DayView,
   WeekView,
+  MonthView,
   Appointments,
+  ViewSwitcher,
+  Toolbar,
 } from "@devexpress/dx-react-scheduler-material-ui";
 import { AgendaProps } from "./agenda.type";
 
 export const Agenda = ({ schedulerData, currentDate }: AgendaProps) => (
   <Paper>
     <Scheduler data={schedulerData}>
-      <ViewState currentDate={currentDate} />
+      <ViewState currentDate={currentDate} defaultCurrentViewName="Week" />
+      <DayView startDayHour={0} endDayHour={24} />
       <WeekView startDayHour={10} endDayHour={24} />
+      <MonthView />
       <Appointments />
+      <Toolbar />
+      <ViewSwitcher />
     </Scheduler>
   </Paper>
 );
