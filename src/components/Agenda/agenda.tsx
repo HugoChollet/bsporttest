c864d9894f199a6834542e6d26fe1255954523a7
HugoChollet <tibolescabo@gmail.com>
2023-03-21 11:48:15 +0100

    add DateNavigator and Tooltip appointement

--- src/components/Agenda/agenda.tsx
+++ src/components/Agenda/agenda.tsx
@@ -6,7 +6,10 @@ import {
   DayView,
   WeekView,
   MonthView,
+  DateNavigator,
   Appointments,
+  AppointmentTooltip,
+  AppointmentForm,
   ViewSwitcher,
   Toolbar,
 } from "@devexpress/dx-react-scheduler-material-ui";
@@ -15,13 +18,19 @@ import { AgendaProps } from "./agenda.type";
 export const Agenda = ({ schedulerData, currentDate }: AgendaProps) => (
   <Paper>
     <Scheduler data={schedulerData}>
-      <ViewState currentDate={currentDate} defaultCurrentViewName="Week" />
+      <ViewState
+        defaultCurrentDate={currentDate}
+        defaultCurrentViewName="Week"
+      />
       <DayView startDayHour={0} endDayHour={24} />
       <WeekView startDayHour={10} endDayHour={24} />
       <MonthView />
       <Appointments />
+      <AppointmentTooltip showCloseButton showOpenButton />
+      {/* <AppointmentForm readOnly /> */}
       <Toolbar />
       <ViewSwitcher />
+      <DateNavigator />
     </Scheduler>
   </Paper>
 );
