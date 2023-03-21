b5304daf6d005d4f1112d274f213df0b64c39def
HugoChollet <tibolescabo@gmail.com>
2023-03-21 16:04:13 +0100

    add Activity data to the appointement

--- src/pages/AgendaScreen.tsx
+++ src/pages/AgendaScreen.tsx
@@ -4,28 +4,38 @@ import { Agenda } from "@/components/Agenda/agenda";
 import { getData } from "./api/getData";
 import { AgendaProps } from "@/components/Agenda/agenda.type";
 
-const GETAPI =
+const GET_OFFER_API =
   "https://api.staging.bsport.io/api/v1/offer/?min_date=2019-03-01&max_date=2019-03-31&company=6";
 const currentDate = "2019-03-30";
 
 export default function AgendaScreen() {
-  const [offer, setOffer] = useState(getData(GETAPI));
+  const [offer, setOffer] = useState(getData(GET_OFFER_API));
   const [agendaData, setAgendaData] = useState<AgendaProps["schedulerData"]>();
 
   useEffect(() => {
-    offer.then((data) => {
+    offer.then(async (data) => {
       setAgendaData(
-        data.results.map((time) => {
-          const endDate = new Date(time.date_start);
-          endDate.setUTCMinutes(time.duration_minute);
+        await Promise.all(
+          data.results.map(async (time) => {
+            const activityName = getData(
+              "https://api.staging.bsport.io/api/v1/meta-activity/?id__in=" +
+                time.activity +
+                "&company=6"
+            ).then((response) => response.results[0].name);
+            const endDate = new Date(time.date_start);
 
-          return {
-            startDate: new Date(time.date_start),
-            endDate: endDate,
-            title: "Appointement",
-          };
-        })
+            endDate.setUTCMinutes(time.duration_minute);
+
+            return {
+              startDate: new Date(time.date_start),
+              endDate: endDate,
+              title: await activityName,
+            };
+          })
+        )
       );
+
+      console.log(agendaData);
     });
   }, [offer]);
 
