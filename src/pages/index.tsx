a0a352fa61529ac5d66726c88f41cf76c0c1b027
HugoChollet <tibolescabo@gmail.com>
2023-03-21 12:18:49 +0100

    add link to github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,11 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { useState } from "react";
-import { getData } from "./api/getData";
-import { Agenda } from "@/components/Agenda/Agenda";
+import AgendaScreen from "./AgendaScreen";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const GETAPI =
-  "https://api.staging.bsport.io/api/v1/offer/?min_date=2019-03-17&max_date=2019-03-31&company=6";
-
 export default function Home() {
-  const [offer, setOffer] = useState(getData(GETAPI));
-
-  offer.then((data) => {
-    console.log(data);
-  });
-
   return (
     <>
       <Head>
@@ -35,7 +24,7 @@ export default function Home() {
           </p>
           <div>
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://github.com/HugoChollet/bsporttest"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -43,19 +32,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-        {/* <Agenda /> */}
-
+        <AgendaScreen />
         <div className={styles.grid}>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
