198c58545caeab3932e5265147e52add5de75694
HugoChollet <tibolescabo@gmail.com>
2023-03-26 22:25:53 +0200

    edit createAppointement to render all successful data

--- src/pages/api/createAppointement.tsx
+++ src/pages/api/createAppointement.tsx
@@ -14,27 +14,23 @@ export const createAppointement = async ({
   const endDate = new Date(time.date_start);
 
   endDate.setUTCMinutes(time.duration_minute);
-  if (
-    !activityName.find((item: any[]) => item[0] === time.activity) ||
-    !coachName.find((item: any[]) => item[0] === time.coach) ||
-    !establishmentName.find((item: any[]) => item[0] === time.establishment)
-  ) {
-    return {
-      startDate: new Date(time.date_start),
-      endDate: endDate,
-      activity: "Searching",
-      coach: "Searching",
-      establishment: "Searching",
-    };
-  }
-
   return {
     startDate: new Date(time.date_start),
     endDate: endDate,
-    activity: activityName.find((item: any[]) => item[0] === time.activity)[1],
-    coach: coachName.find((item: any[]) => item[0] === time.coach)[1],
-    establishment: establishmentName.find(
+    activity: !activityName.find(
+      (item: any[]) => item[0] === time.meta_activity
+    )
+      ? "Searching"
+      : activityName.find((item: any[]) => item[0] === time.meta_activity)[1],
+    coach: !coachName.find((item: any[]) => item[0] === time.coach)
+      ? "Searching"
+      : coachName.find((item: any[]) => item[0] === time.coach)[1],
+    establishment: !establishmentName.find(
       (item: any[]) => item[0] === time.establishment
-    )[1],
+    )
+      ? "Searching"
+      : establishmentName.find(
+          (item: any[]) => item[0] === time.establishment
+        )[1],
   };
 };
